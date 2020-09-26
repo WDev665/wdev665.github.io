e58ad9e0c29bb3420bf587c132e8a91d2ca75b77
WDev665 <programming665@gmail.com>
2020-09-26 20:09:38 +0300

    Sign in form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+/*Скрипт форми*/
+let isPassVisible = false;
+
+$('.password-status').on('click', function(){
+    if(!isPassVisible){
+        $('input[name="password"]').attr('type', 'text');
+        isPassVisible = true;
+    }else if(isPassVisible){
+        $('input[name="password"]').attr('type', 'password');
+        isPassVisible = false;
+    }
+});
+
 /*Скріпт анімованої загрузки*/
