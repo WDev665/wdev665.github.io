8a2e6d042fa9e0c1296318dbe8664a0641aa6dd1
eternal-envy <romaklor228@gmail.com>
2020-10-02 21:21:29 +0300

    commented func

---
+++ js/profile-script.js
@@ -0,0 +1,22 @@
+let startPageBackground = document.querySelector('.start-page-background');
+function fadeOut(){
+    startPageBackground.style.opacity = 1;
+    /*
+    (function fade() {
+        if ((startPageBackground.style.opacity -= .05) < 0) {
+            startPageBackground.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+    */
+    $('.start-page-background').fadeOut(2000);
+}
+function textMovement(){
+    $('h1').fadeOut(2000);
+}
+setTimeout(function () {
+    document.querySelector('.start-page').style.zIndex = 0;
+},4500);
+setTimeout(textMovement,3000);
+setTimeout(fadeOut, 3000);
