170289c69bd38a07338cb6f466fa25408cc1e5a6
WDev665 <programming665@gmail.com>
2020-10-02 20:31:25 +0300

    authorizing

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let isPassVisible = false;
 $('.password-status').on('click', function(){
     if(!isPassVisible){
         $('input[name="password"]').attr('type', 'text');
-        $('')
+        $('.password-status').css('background-image','url(img/pass-icon-h.png)');
         isPassVisible = true;
     }else if(isPassVisible){
         $('input[name="password"]').attr('type', 'password');
+        $('.password-status').css('background-image','url(img/pass-icon.png)');
         isPassVisible = false;
     }
 });
@@ -41,8 +42,57 @@ setTimeout(textMovement,3000);
 setTimeout(fadeOut, 3000);
 
 /*Авторизація*/
+let users;
+let userId;
+let userFounded = false;
+let userAllowed = false;
+
 $.getJSON("db/users.json", function(json) {
-    console.log(json); //туть
+    console.log(json);
+    users = json.users;
 });
+
+function authorizing(){
+    let login = $('input[name="login"]').val();
+    let pass = $('input[name="password"]').val();
+
+    for(let i = 0; i < users.length; i++) {
+        if (users[i]['userName'] === login) {
+            userFounded = true;
+            userId = i;
+
+            checkPass(userId, pass);
+            return false;
+        }
+    }
+
+    function checkPass(id, pass){
+        if(users[id]['userPassword'] === pass){
+            userAllowed = true;
+            console.log('userAllowed');
+
+            $('.login-status').css('background-image', 'url(img/login-icon-allowed.png)');
+            if(isPassVisible){
+                $('.password-status').css('background-image', 'url(img/pass-icon-h-allowed.png)');
+            }else{
+                $('.password-status').css('background-image', 'url(img/pass-icon-allowed.png)');
+            }
+        }else{
+            console.log('wrongPassword');
+            $('.login-status').css('background-image', 'url(img/login-icon-allowed.png)');
+
+            if(isPassVisible){
+                $('.password-status').css('background-image', 'url(img/pass-icon-h-denied.png)');
+            }else{
+                $('.password-status').css('background-image', 'url(img/pass-icon-denied.png)');
+            }
+        }
+    }
+
+    if(!userFounded){
+        console.log('unavailableUser');
+        $('.login-status').css('background-image', 'url(img/login-icon-denied.png)');
+    }
+}
 
 /*Функція зворотньої анімації*/
