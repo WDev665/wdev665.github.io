1f7631b5f23e938683f0c4b5307bce4590b32cd3
eternal-envy <romaklor228@gmail.com>
2020-10-03 22:47:25 +0300

    header

--- js/profile-script.js
+++ js/profile-script.js
@@ -1,4 +1,4 @@
-/*let startPageBackground = document.querySelector('.start-page-background');
+let startPageBackground = document.querySelector('.start-page-background');
 function fadeOut(){
     startPageBackground.style.opacity = 1;
     /*
@@ -9,17 +9,30 @@ function fadeOut(){
             requestAnimationFrame(fade);
         }
     })();
-
+    */
     $('.start-page-background').fadeOut(2000);
     $('.fade').fadeOut(2000);
     $('.start-page').fadeOut(2000);
 }
 function textMovement() {
-    $('h1').fadeOut(2000);
+    $('.start-page-logo').fadeOut(2000);
 }
 setTimeout(function () {
     document.querySelector('.start-page').style.zIndex = 0;
 },4500);
 setTimeout(textMovement,3000);
 setTimeout(fadeOut, 3000);
-*/
+document.querySelector('.exit').addEventListener('click',function () {
+    document.querySelector('.start-page').style.zIndex = 10;
+    setTimeout(pageFadeIn,10);
+});
+function pageFadeIn() {
+    $('.start-page-background').fadeIn(2000);
+    $('.fade').fadeIn(2000);
+    $('.start-page').fadeIn(2000);
+    $('.start-page-logo').fadeIn(2000);
+    setTimeout(redirect,2200);
+}
+function redirect() {
+    window.location.replace('index.html');
+}
